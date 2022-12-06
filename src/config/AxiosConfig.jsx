572cf0cd35bc9e4add050bb6a2575ad1a7ee7d2c
danpanorama <62213659+danpanorama@users.noreply.github.com>
2022-12-06 18:00:26 +0200

    change to http from https

--- src/config/AxiosConfig.jsx
+++ src/config/AxiosConfig.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
 //   baseURL: "https://samia.co.il/",
   // baseURL: "https://morcmario.com/",
-  baseURL: "https://54.95.206.170:3040/",
+  baseURL: "http://54.95.206.170:3040/",
 
  
   
